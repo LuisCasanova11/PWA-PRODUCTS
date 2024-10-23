e371dfb091c84684e6300e0c9fb97d8463be3a29
Luis A. Casanova <132479713+LuisCasanova11@users.noreply.github.com>
2024-10-22 20:28:45 -0600

    offline

--- products-front/pwa-products-app/src/services/movieService.js
+++ products-front/pwa-products-app/src/services/movieService.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { openDB } from 'idb';
 
 const API_URL = 'https://localhost:5000/api/products';
 
@@ -6,11 +7,6 @@ export const ProductService = {
   // Obtener todas las películas y series
   getProducts() {
     return axios.get(API_URL);
-  },
-  
-  // Agregar una nueva película o serie
-  addMovie(movie) {
-    return axios.post(API_URL, movie);
   },
   
   // Actualizar una película o serie
@@ -21,5 +17,35 @@ export const ProductService = {
   // Eliminar una película o serie
   deleteMovie(id) {
     return axios.delete(`${API_URL}/${id}`);
+  },
+
+  async addMovie(movie) {
+    if (navigator.onLine) {
+      return axios.post(API_URL, movie);
+    } else {
+      await saveProductOffline(movie);
+      await registerSync();
+      return Promise.resolve({ message: 'Producto guardado offline' });
+    }
   }
 };
+
+async function saveProductOffline(product) {
+  const db = await openDB('offline-store', 1, {
+    upgrade(db) {
+      db.createObjectStore('products', { keyPath: 'id', autoIncrement: true });
+    },
+  });
+  await db.put('products', product);
+}
+
+async function registerSync() {
+  if ('serviceWorker' in navigator && 'SyncManager' in window) {
+    const registration = await navigator.serviceWorker.ready;
+    try {
+      await registration.sync.register('sync-products');
+    } catch (err) {
+      console.error('Error al registrar la sincronización:', err);
+    }
+  }
+}
--- products-front/pwa-products-app/src/services/pushService.js
+++ products-front/pwa-products-app/src/services/pushService.js
@@ -3,10 +3,8 @@ export async function subscribeToPush() {
     try {
       const registration = await navigator.serviceWorker.ready;
       
-      // Convertir la clave pública VAPID
       const applicationServerKey = urlBase64ToUint8Array('BGm5bKOxOF91wRWsXQrUd9J9C8j7OJn03nrtqW3iL8gJo44bPdOCzxajr1DCDVDyTMQzh3LUQCK-qb4e7N-Hveg');
   
-      // Suscribirse al Push Manager
       const subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
         applicationServerKey
@@ -31,7 +29,6 @@ export async function subscribeToPush() {
     }
   }
   
-  // Convertir la clave pública VAPID en Uint8Array
   export function urlBase64ToUint8Array(base64String) {
     const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
     const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
